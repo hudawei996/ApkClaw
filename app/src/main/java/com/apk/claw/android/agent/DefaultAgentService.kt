761d73be58a33d5b2520bc058b8715f6ddafb115
hudawei996 <262489227@qq.com>
2026-04-27 10:10:50 +0800

    test

--- app/src/main/java/com/apk/claw/android/agent/DefaultAgentService.kt
+++ app/src/main/java/com/apk/claw/android/agent/DefaultAgentService.kt
@@ -29,6 +29,9 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
 
+/**
+ * 默认的 LLM 代理服务实现
+ */
 class DefaultAgentService : AgentService {
 
     companion object {
